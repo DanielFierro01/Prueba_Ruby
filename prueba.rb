e64b445ceb4d0e256dd4670db6e772c408edbd4b
Daniel Fierro <dfierropalma@gmail.com>
2021-03-24 18:30:04 -0300

    estructura base de datos

--- prueba.rb
+++ prueba.rb
@@ -1,1 +1,17 @@
-asdfadfasdf
+require "uri"
+require "net/http"
+require "json"
+def request (adress, R81S5qlaZAWZAFj7zZtSUgq4bw0dGK125dz4b5rk)
+
+
+url= URI ("#{adress}&api_key=#{api_key}") 
+https = Net::HTTP.new(url.host, url.port)
+https.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+
+response = https.request(request)
+response.read_body
+JSON.parse response.read_body 
+end
+
