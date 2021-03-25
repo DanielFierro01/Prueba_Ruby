e7e5210a4213a8c58a6888245bf6d37233e71be5
Daniel Fierro <dfierropalma@gmail.com>
2021-03-25 13:10:27 -0300

    terminado

--- prueba.rb
+++ prueba.rb
@@ -1,34 +1,43 @@
 require "uri"
-require "net/http"
+
+require "net/https"
+
 require "json"
-def request (adress,api_key="R81S5qlaZAWZAFj7zZtSUgq4bw0dGK125dz4b5rk")
 
-
-    url= URI ("#{adress}&api_key=#{api_key}") 
+def request(address, api_key="R81S5qlaZAWZAFj7zZtSUgq4bw0dGK125dz4b5rk") 
+    url = URI("#{address}&api_key=#{api_key}")
     https = Net::HTTP.new(url.host, url.port)
     https.use_ssl = true
-
     request = Net::HTTP::Get.new(url)
-
     response = https.request(request)
     response.read_body
-    JSON.parse response.read_body 
+    JSON.parse response.read_body  
+
 end
+
 
 def build_web_page(request)
 
-    imagenes= request.map {|x| x["img_src"]}
+    fotos_satelite = request.map{ |filter| filter['img_src'] } 
 
-    estructura= "<html>\n<head>\n</head>\n\t<body>\n\t\t<ul>"
-    imagenes.each do |z|
-        html+="\n\t\t\t<li><img src='#{img}'></li>\n"
+    pagina = "<html>\n<head>\n</head>\n\t<body>\n\t\t<ul>" 
+
+    fotos_satelite.each do |img|
+
+    pagina += "\n\t\t\t<li><img src='#{img}'></li>\n" 
 
     end
 
-    html+="\t\t</ul>\n\t</body>\n</html>"
-    return html
+    pagina += "\t\t</ul>\n\t</body>\n</html>" 
+
+    return pagina
+
 end
 
-fotos=build_web_page(request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000")["fotos"])
-File.write("fotos_satelite.html",fotos.to_s)
+photos = build_web_page(request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000')["photos"])
+
+File.write('imagenes_satelite.html', photos.to_s)
+
+
+
 
